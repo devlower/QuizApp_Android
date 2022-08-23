a704d4d4bb8ddba96d40b649d74764b3f1995ddb
Tuanne Assenço <tuanne.assenco@gmail.com>
2022-08-23 16:30:47 -0300

    Preparing the UI for the result activity

---
+++ app/src/main/java/com/devlower/quizapp/ResultScreen.kt
@@ -0,0 +1,11 @@
+package com.devlower.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_screen)
+    }
+}
